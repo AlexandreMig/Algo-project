0c2af55676e520c1cd4974600dbcc885aff91b8a
dyllan britz <britz@usf.edu>
2023-04-17 20:36:55 -0400

    still debugging
    will finish debugging tomorrow but this is the framework

--- DFS.cs
+++ DFS.cs
@@ -1,10 +1,55 @@
 // Input: A graph G and a vertex v of G
 // Output: All vertices reachable from v //labeled as discovered
-procedure DFS(G,v) {
-  label v as "discovered"
-  for all vertex w in G.adjacentVertices(v) {
-    if (vertex w is not labeled as "discovered") {
-      DFS(G, w)
-     }
-   }
+public void DFS()
+{
+  for(int v = 1; v < graphsize; v++)
+  {
+    if(!graphlinks[v].visited)
+    {
+      DFS_checks(v);
+    }
+  }
+  EdgeNode current = null;
+  EdgeNode adjacent = null;
+  Console.WriteLine("DFS output:");
+  for(int i = 1; i <= graphSize; i++)
+  {
+      current = graphlinks[i].head;
+      if(current.next != null)
+      {
+        adjacent = current.next;
+      }
+      while (current != null && adjacent != null)
+      {
+        Console.Write(current.graphlinks);
+        while(adjacent != null)
+        {
+          string node_printer = "<" + adjacent.graphlinks + ">";
+          Console.Write(line);
+          adjacent = graphlinks.next;
+          if( i = 3)
+          {
+            Console.WriteLine();
+          } 
+        }
+      }
+    Console.WriteLine(weight);
+  }
+}
+
+private void DFS_Checks(int v)
+{
+  graphSize[v].visited = true;
+  weight = v + weight;
+  int w = vertex ;
+  Edge n = null;
+  if (graphlinks[v].head.next != null)
+  {
+    n = graphlinks[v].head.next;
+    w = n.Node_Adjacent;
+  }
+  if (!graphlinks[v].visited)
+  {
+    DFS_checks(w);
+  }
 }
--- main.cs
+++ main.cs
@@ -1,8 +1,26 @@
-/*
-Follow basic parameter
 
-int Algorithm(int[] graph, int vertex, int total){
+
+class GraphList
+{
+    class Edge
+    {
+        public int Node_Adjacent; //adjacent node initialization
+        public Edge next;
+    }
+    class Graph
+    {
+        public bool visited; //boolean call for visited
+        public Edge head;
+        public int weight;
+    }
+    Graph[] graphlinks; //array for input 
+    int graphSize = 9; //size of graph should be 9
+    public GraphList(){}
+}
+
+int Algorithm(int[] graphlinks, int vertex, int total)
+{
 
 }
 
-*/
+
