9f4bffc897c909971a7cac029693b4738a8ef9ac
dyllan britz <britz@usf.edu>
2023-04-18 14:15:48 -0400

    fixed formating to be able to be called

--- DFS.h
+++ DFS.h
@@ -1,55 +1,31 @@
 // Input: A graph G and a vertex v of G
 // Output: All vertices reachable from v //labeled as discovered
-public void DFS()
-{
-    for (int v = 1; v < graphsize; v++)
-    {
-        if (!graphlinks[v].visited)
-        {
-            DFS_checks(v);
-        }
-    }
-    EdgeNode current = null;
-    EdgeNode adjacent = null;
-    Console.WriteLine("DFS output:");
-    for (int i = 1; i <= graphSize; i++)
-    {
-        current = graphlinks[i].head;
-        if (current.next != null)
-        {
-            adjacent = current.next;
-        }
-        while (current != null && adjacent != null)
-        {
-            Console.Write(current.graphlinks);
-            while (adjacent != null)
-            {
-                string node_printer = "<" + adjacent.graphlinks + ">";
-                Console.Write(line);
-                adjacent = graphlinks.next;
-                if (i = 3)
-                {
-                    Console.WriteLine();
-                }
-            }
-        }
-        Console.WriteLine(weight);
-    }
-}
+using System;
 
-private void DFS_Checks(int v)
+int AddWeight(int v, int weight)
 {
-    graphSize[v].visited = true;
-    weight = v + weight;
-    int w = vertex;
-    Edge n = null;
-    if (graphlinks[v].head.next != null)
+    weight = weight + v;
+    return weight;
+}
+static void DFS(int[] G, int v, bool[] visited, int next_i, int weight)
+{
+    visited[next_i] = true;
+    Console.Write("<" + G[si] + ">");
+    for( int i = 0; i < v; i ++)
     {
-        n = graphlinks[v].head.next;
-        w = n.Node_Adjacent;
+        if(i == next_i)
+        {
+            continue;
+        }
+        if( i == 3)
+        {
+            Console.WriteLine();
+        }
+        if(!visited[i] && G[next_i, i] == i)
+        {
+            DFS(G, v, visited, i)
+        }
+
     }
-    if (!graphlinks[v].visited)
-    {
-        DFS_checks(w);
-    }
+    Console.Write("cost of procedure: " + weight);    
 }
--- main.cs
+++ main.cs
@@ -1,26 +1,7 @@
 
-internal class GraphList
+int Algorithm(int[] G, int vertex, int total)
 {
-    private class Edge
-    {
-        public int Node_Adjacent; //adjacent node initialization
-        public Edge next;
-    }
-
-    private class Graph
-    {
-        public bool visited; //boolean call for visited
-        public Edge head;
-        public int weight;
-    }
-    Graph[] graphlinks; //array for input 
-    int graphSize = 9; //size of graph should be 9
-    public GraphList() { }
-
-    int Algorithm(int[] graphlinks, int vertex, int total)
-    {
-        return 0;
-    }
+    return 0;
 }
 
 
