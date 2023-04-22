67b55e11b7ff2960dab5a4d75bc36b9cc2b6c27e
Alexandre <75760260+AlexandreMig@users.noreply.github.com>
2023-04-22 19:06:57 -0400

    BFS

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Algo.Dijkstra;
+using Algo.BFS;
+
+namespace Algo {
+    public class Program
+    {
+        static void Main()
+        {
+            int[] custom = new int[]
+            {
+                4, 1, 3,
+                5, 8, 2,
+                0, 6, 7
+            };
+
+            int[] sample1 = new int[]
+            {
+                1, 3, 4,
+                8, 0, 2,
+                7, 6, 5
+            };
+
+            int[] sample2 = new int[]
+            {
+                1, 3, 4,
+                8, 0, 6,
+                7, 5, 2
+            };
+
+            int[] goalState = new int[]
+            {
+                1, 2, 3,
+                8, 0, 4,
+                7, 6, 5
+            };
+
+            int[] goalStateFile = new int[]
+            {
+                7, 2, 4,
+                5, 0, 6,
+                8, 3, 1
+            };
+
+            int[] example = new int[]
+            {
+                8, 6, 4,
+                2, 1, 3,
+                5, 7, 0
+            };
+
+            DijkstraAlgorithm dijkstra = new DijkstraAlgorithm();
+            BFSAlgorithm bfs = new BFSAlgorithm();
+
+            int totalDijkstra = dijkstra.Dijkstra(sample2, goalState);
+            int totalBFS = bfs.BFS(example, goalStateFile);
+
+            Console.WriteLine($"Result for Dijkstra: {totalDijkstra}");
+            Console.WriteLine($"Result for BFS: {totalBFS}");
+        }
+    }
+}
